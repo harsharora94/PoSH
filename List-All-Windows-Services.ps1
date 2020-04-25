9f49ca37245bdcfcf8eb0860150adad1033911ee
Harsh Arora <64034129+harsharora94@users.noreply.github.com>
2020-04-25 14:44:33 +0530

    Retrieving All Windows Services

---
+++ List-All-Windows-Services.ps1
@@ -0,0 +1,26 @@
+﻿# --------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#  Name        		:	List-All-Windows-Services.ps1
+#  Author      		:	Harsh Arora
+#  Description     	:	This powershell script uses Get-WmiObject to retrieve all services, which are instances of the Win32_Service Class and export it to CSV format. 
+#---------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+$Logfile = "$env:SystemDrive\ListAllWindowsServices.log"
+$ExportServicesCSV = "$env:SystemDrive\AvailableWindowsServices.csv"
+
+function LogWrite([string]$logstring)
+{
+	$DateTime = Get-Date -Format g
+	Add-content $Logfile -value "$DateTime $logstring"
+}
+
+$Return = Get-WMIObject Win32_Service | Select-Object DisplayName, Description, State, StartMode, StartName, PathName
+
+If($? -eq $true)
+{
+   Get-WMIObject Win32_Service | Select-Object DisplayName, Description, State, StartMode, StartName, PathName | Export-CSV -NoType $ExportServicesCSV
+   LogWrite "Exporting of all Windows Services using Get-WmiObject in CSV format at location: '$ExportServicesCSV' and returned: $?" 
+}
+else
+{
+   LogWrite "Failed to export all Windows Services using Get-WmiObject in CSV format at location: '$ExportServicesCSV' and returned: $?" 
+}
